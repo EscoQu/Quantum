6c61a22a35944a20769da8666d1e4c52bf7e41a6
PacoGalvez <fgalvez@nebrija.es>
2021-12-21 14:25:50 +0000

    "Commit Inicial"

--- reuniones.py
+++ reuniones.py
@@ -3,11 +3,13 @@ from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
 
 sampler = EmbeddingComposite(DWaveSampler())
+sampler = EmbeddingComposite(DWaveSampler(solver='DW_2000Q_6'))
+sampler = EmbeddingComposite(DWaveSampler(solver=dict(topology__type='pegasus')))
 
 ##############################################################
 ##  horario     --> 1: Trabajo      0: fuera de horario
 ##  ubicacion   --> 1: Presencial   0: Remoto
-##  duracion    --> 1: Larga        0: Corta
+##  duracion    --> 1: Corta        0: Larga
 ##  asistencia  --> 1: Obligatoria  1: Opcional
 ##############################################################
 
@@ -17,7 +19,7 @@ def planifica(horario, ubicacion, duracion, asistencia):
         return (ubicacion and asistencia)
     else:
         # Fuera de horario
-        return (not ubicacion and not duracion)
+        return (not ubicacion and duracion)
 
 csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.BINARY)
 csp.add_constraint(planifica, ['horario', 'ubicacion', 'duracion', 'asistencia'])
@@ -37,7 +39,7 @@ for sample, energy, occurences in response.data(['sample', 'energy', 'num_occurr
     # if energy == min_energy:
     horario = 'Horario de trabajo' if sample['horario'] else 'Fuera de horario'
     ubicacion = 'presencial' if sample['ubicacion'] else 'remota'
-    duracion = 'larga' if sample['duracion'] else 'corta'
+    duracion = 'corta' if sample['duracion'] else 'larga'
     asistencia = 'obligatoria' if sample['asistencia'] else 'opcional'
     print("{}: {} sesion de tipo {}, de duracion {} con asistencia {}"
                 .format(occurences, horario, ubicacion, duracion, asistencia))
